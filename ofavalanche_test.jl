c1d5bb8b6d87ac5d13732d330757a4c655afadf0
Alan Correa <40885942+thealanjason@users.noreply.github.com>
2024-03-06 18:23:46 +0100

    Update ofavalanche_test.jl
    Delete old code

--- ofavalanche_test.jl
+++ ofavalanche_test.jl
@@ -5,30 +5,12 @@ Pkg.instantiate()
 using OpenFOAM_com_jll
 
 cd("synthetic-slope-elliptic-hemisphere/")
-## This following calls fail because the libraries used by the executables
-## cannot find the WM_PROJECT_DIR path in the environment of the command
+
 run(gridToSTL())
 run(pMesh())
 run(makeFaMesh())
 # restore0dir
-# rm("0", recursive=true)
 cp("0.orig", "0")
 run(releaseAreaMapping())
 run(faSavageHutterFoam())
 
-## When we add the WM_PROJECT_DIR with the correct path to the Cmd env
-## the calls work as expected
-
-# This is the path the etc folder is searched for by OpenFOAM libraries
-# dirname(OpenFOAM_com_jll.openfoam_etc)
-# So we set this path to the WM_PROJECT_DIR variable in the Julia script 
-# and add it to the environment for each call
-# WM_PROJECT_DIR = dirname(OpenFOAM_com_jll.openfoam_etc) 
-# run(addenv(gridToSTL(), "WM_PROJECT_DIR"=>WM_PROJECT_DIR));
-# run(addenv(pMesh(), "WM_PROJECT_DIR"=>WM_PROJECT_DIR));
-# run(addenv(makeFaMesh(), "WM_PROJECT_DIR"=>WM_PROJECT_DIR));
-# # restore0dir
-# rm("0", recursive=true)
-# cp("0.orig", "0")
-# run(addenv(releaseAreaMapping(), "WM_PROJECT_DIR"=>WM_PROJECT_DIR));
-# run(addenv(faSavageHutterFoam(), "WM_PROJECT_DIR"=>WM_PROJECT_DIR));
